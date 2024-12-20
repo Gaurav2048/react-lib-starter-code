f8c087c59c84f6d438afe0313d20ff9ba7f5dd8c
Gaurav2048 <gunjankalita36@gmail.com>
2024-12-20 14:03:39 +0530

    Update test

--- src/MyComponent.test.tsx
+++ src/MyComponent.test.tsx
@@ -2,5 +2,5 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import MyComponent from "./MyComponent";
 
 test('renders a button with the given label', () => {
-  render(<MyComponent text="Click Me" />); // Render the component
+  render(<MyComponent text="Dummy Text" />); // Render the component
 });
